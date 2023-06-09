1ddfa5f3c908ad5207d9d7ab5f2c98169d874e24
Mikhail <mishagrigoryan@gmail.com>
2023-06-08 23:29:14 -0400

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,7 +1,1 @@
-database_config = {
-    'dbname': 'postgres',
-    'user': 'postgres',
-    'password': 'Manonthemoon123',
-    'host': 'database-1.cueq5a3aruqx.us-east-2.rds.amazonaws.com',
-    'port': '5432' 
-}
+
