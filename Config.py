f266354c8f19725ff9669ab044631c8581c90aa0
Misha <mishagrigoryan@gmail.com>
2023-06-09 23:19:05 -0400

    wd

--- Config.py
+++
@@ -1,8 +0,0 @@
-
-database_config = {
-    "dbname" : "postgres",
-    "user" : "postgres",
-    "password" : "Manonthemoon123",
-    "host" : "database-1.cueq5a3aruqx.us-east-2.rds.amazonaws.com", # or your host
-    "port" : "5432" # or your port
-}
